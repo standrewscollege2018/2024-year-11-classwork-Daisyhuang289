39c5dadf257b9a936a120c971476a16c8b4c0788
Daisyhuang289 <158853168+Daisyhuang289@users.noreply.github.com>
2024-02-09 10:11:30 +1300

    Checking for negative numbers

--- doublenumber.py
+++ doublenumber.py
@@ -4,8 +4,11 @@ keepasking = True
 while keepasking:
     # Take number input as a float and then print double the number
     try:
-        num = float(input())
-        keepasking = False
+        num = float(input("Enter a positive number: "))
+        if num >= 0:
+            keepasking = False
+        else:
+            print("Please enter a positive number")
     except ValueError:
         print("Please enter a number")
 print(f"{num} doubled is {num * 2}")
