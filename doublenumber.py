fb76a856e7cefe10159d0e464307c614cb35af69
Daisyhuang289 <158853168+Daisyhuang289@users.noreply.github.com>
2024-02-08 15:16:29 +1300

    Asking the question in a loop

--- doublenumber.py
+++ doublenumber.py
@@ -1,8 +1,11 @@
 ''' This program takes a number as input and doubles it '''
 
-# Take number input as a float and then print double the number
-try:
-    num = float(input())
-    print(num*2)
-except ValueError:
-    print("Please enter a number")
+keepasking = True
+while keepasking:
+    # Take number input as a float and then print double the number
+    try:
+        num = float(input())
+        print(num*2)
+        keepasking = False
+    except ValueError:
+        print("Please enter a number")
