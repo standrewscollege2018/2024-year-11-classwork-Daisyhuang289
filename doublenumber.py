222b2cc3c143312697825bdb4461f11d7cd0c8cf
Daisyhuang289 <158853168+Daisyhuang289@users.noreply.github.com>
2024-02-09 10:04:01 +1300

    using f string to print a statement

--- doublenumber.py
+++ doublenumber.py
@@ -5,7 +5,7 @@ while keepasking:
     # Take number input as a float and then print double the number
     try:
         num = float(input())
-        print(num*2)
         keepasking = False
     except ValueError:
         print("Please enter a number")
+print(f"{num} doubled is {num * 2}")
