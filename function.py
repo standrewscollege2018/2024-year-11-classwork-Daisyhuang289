fd323b70f4ddb1a485bbae7f4142dfb6f19dd60f
Daisyhuang289 <158853168+Daisyhuang289@users.noreply.github.com>
2024-02-12 09:01:35 +1300

    multiply 2 numbers

---
+++ function.py
@@ -0,0 +1,12 @@
+''' This program demostrates how a function works '''
+
+''' multiply() multiples 2 numbers and returns result '''
+def multiply(a, b):
+    return a * b
+
+# Get 2 numbers from user
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+
+# Display the product of the 2 numbers
+print(multiply(num1, num2))
