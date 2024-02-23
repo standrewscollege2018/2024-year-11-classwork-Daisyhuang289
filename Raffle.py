3dc1425b238b962bf103e8d32a4f78833e79fefc
Daisyhuang289 <158853168+Daisyhuang289@users.noreply.github.com>
2024-02-23 18:15:59 +1300

    error catching

--- Raffle.py
+++ Raffle.py
@@ -6,10 +6,20 @@ while ask == False:
     name = input("Please enter a name: ")
     if name.strip() == "":
         print("Enter a existing name")
-    elif name.lower() == "stop":
+    elif name.lower() == "end":
         ask = True
     else:
         lst.append(name)
-prize = input("Enter the prize: ")
+while ask == True:
+    try:
+        prize = int(input("Enter the prize: "))
+        ask = False
+    except ValueError:
+        print("Enter a money value")
 value = randint(0, len(lst)-1)
-print(f"{lst[value]} has won the raffle of {prize}")
+if len(lst) == 1:
+    print(f"1 person has entered this raffle")
+elif len(lst) > 1:
+    print(f"{len(lst)} people has entered this raffle")
+print(f"The prize is ${prize}")
+print(f"{lst[value]} has won the raffle of ${prize}")
