c304a19fbca9640f2537e8df63e76c39882468b4
Daisyhuang289 <158853168+Daisyhuang289@users.noreply.github.com>
2024-02-26 09:12:33 +1300

    Error catching and different format

--- Raffle.py
+++ Raffle.py
@@ -1,25 +1,20 @@
 from random import randint
-print("Hello,")
+print("Welcome to the raffle program")
+prize = input("What is the prize being raffled? ")
+value = int(input("What is the value of the {prize} (do not enter the $ sign) "))
 lst = []
 ask = False
 while ask == False:
-    name = input("Please enter a name: ")
+    name = input("Enter name of entrant: ")
     if name.strip() == "":
         print("Enter a existing name")
     elif name.lower() == "end":
         ask = True
     else:
         lst.append(name)
-while ask == True:
-    try:
-        prize = int(input("Enter the prize: "))
-        ask = False
-    except ValueError:
-        print("Enter a money value")
-value = randint(0, len(lst)-1)
+num = randint(0, len(lst)-1)
 if len(lst) == 1:
-    print(f"1 person has entered this raffle")
+    print(f"There is 1 person in the draw for the {prize}")
 elif len(lst) > 1:
-    print(f"{len(lst)} people has entered this raffle")
-print(f"The prize is ${prize}")
-print(f"{lst[value]} has won the raffle of ${prize}")
+    print(f"There are {len(lst)} people in the draw for the {prize}")
+print(f"And the winner of the {prize}, valued at ${value} is... {lst[num]}!")
